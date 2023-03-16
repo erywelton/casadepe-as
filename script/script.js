77749718e7f04fe03bdabf3cedb402b4e712497a
erywelton <127966664+erywelton@users.noreply.github.com>
2023-03-15 21:57:01 -0300

    commit inicial
    meu primeiro projeto

---
+++ script/script.js
@@ -0,0 +1,65 @@
+alert ('Bem Vindos a Casa das Oficinas');
+
+var nome = (prompt('digite seu nome '));
+var tel = (prompt('Digite seu Telefone para contato '));
+var email = (prompt('Digite seu email'));
+console.log(`Nome: ` + (nome));
+console.log('Telefone: ' + (tel));
+console.log('Email: ' + (email));
+alert ('seja bem vindo ' +(nome));
+//
+function mostrar() {
+    let a = document.getElementById('bum')
+    alert('Você comprou o Pneu Formula 295/80 R 22.5 por R$: 1589.99. Compra Finalizada!')
+    console.log('O cliente comprou o Pneu Formula 295/80 R22.5 por R$: 1589.99. Compra Finalizada!')
+}
+function mostrar1 () {
+    let b = document.getElementById('bum1')
+    alert('Oléo Mobil Delvac 15w 40 por R$: 499.99 Compra Finalizada')
+    console.log('O cliente comprou o Oléo Mobil Delvac 15w 40 por R$: 499.99. Compra Finalizada!')
+}
+function mostrar2() {
+    let c = document.getElementById('bum2')
+    alert('Oléo Lubrax 15w 40 por R$: 399.99. Compra Finalizada!')
+    console.log('O cliente comprou o Oléo Lubrax 15w 40 por R$: 399.99. Compra Finalizada!')
+}
+function mostrar3(){
+    let d = document.getElementById('bum3')
+    alert('Filtro de Oléo Man Filter por R$: 199.99 Compra Finalizada')
+    console.log('O cliente comprou o Oléo Man Filter por R$: 199.99. Compra Finalizada!')
+}
+function mostrar4() {
+    let e = document.getElementById('bum4')
+    alert('Kit Embreagem 1620 por R$: 999.00. Compra Finalizada!')
+    console.log('O cliente comprou o Kit Embreagem 1620 por R$: 999.00. Compra Finalizada!')
+}
+function mostrar5() {
+    let f = document.getElementById('bum5')
+    alert('Motor de partida 1620 por R$: 2699.99. Compra Finalizada!')
+    console.log('O cliente comprou o Motor de partida 1620 por:R$ 2699.99. Compra Finalizada!')
+}
+function mostrar6() {
+    let g = document.getElementById('bum6')
+    alert('Cuica de freio boriuder por R$: 799.99. Compra Finalizada!')
+    console.log('O cliente comprou a Cuica de freio boriuder por R$: 799.99. Compra Finalizada!')
+}
+function mostrar7() {
+    let h = document.getElementById('bum7')
+    alert('Roda de Caminhão 22.5 por R$: 899.99. Compra Finalizada!')
+    console.log('O cliente comprou a Roda de Caminhão 22.5 por R$: 899.99. Compra Finalizada!')
+}
+function mostrar8() {
+    let i = document.getElementById('bum8')
+    alert('Você solicitou um Orçamento! Vamos entrar em contato.')
+    console.log('O cliente solicitou o orçamento do motor do caminhão dele. entrar em contato pra fazer o orçamento do Cliente.')
+}
+function mostrar9() {
+    let j = document.getElementById('bum9')
+    alert('Você solicitou um Orçamento! Vamos entrar em contato.')
+    console.log('O cliente solicitou o orçamento da suspensão do caminhão dele. entrar em contato pra fazer o orçamento do Cliente.')
+}
+function mostrar10() {
+    let l = document.getElementById('bum10')
+    alert('Você solicitou um Orçamento! Vamos entrar em contato.')
+    console.log('O cliente solicitou o orçamento da caixa de marcha do caminhão dele. entrar em contato pra fazer o orçamento do Cliente.')
+}
